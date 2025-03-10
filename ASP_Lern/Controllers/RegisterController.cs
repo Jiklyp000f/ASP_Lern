5a77a035a1581369ff31b1bda1231d5887a2dd4c
Pooof Jikly <jiklyp000f@yandex.ru>
2025-03-10 21:08:17 +0300

    добавил валидацию при регистрации, надо добавить БД куда будут сохраняться данные после регистрации

---
+++ ASP_Lern/Controllers/RegisterController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_Lern.Controllers;
+
+public class RegisterController : Controller
+{
+    // GET: /Register/
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    // POST: /Register/
+    [HttpPost]
+    [ValidateAntiForgeryToken] // Добавьте защиту от CSRF
+    public IActionResult Index( RegisterController model )
+    {
+        if ( !ModelState.IsValid )
+        {
+            // Возвращаем форму с ошибками
+            return View( model );
+        }
+
+        // Логика сохранения данных (например, в БД)
+        // Пример:
+        // _context.Users.Add(new User { ... });
+        // _context.SaveChanges();
+
+        return RedirectToAction( "Success" ); // Перенаправляем на успешную страницу
+    }
+}
---
+++ ASP_Lern/Models/RegisterModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASP_Lern.Models;
+
+public class RegisterModel
+{
+    [Required(ErrorMessage = "Username is reqired")]
+    public string Username { get; set; }
+
+    [Required(ErrorMessage = "Email is reqired")]
+    [EmailAddress(ErrorMessage = "Invalid email format")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "Password is reqired")]
+    public string Password { get; set; }
+
+}
